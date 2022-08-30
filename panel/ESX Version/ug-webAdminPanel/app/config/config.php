371b566cc2e1daa636825dfd141270c033a1dc42
UgScripts <112483241+UgScripts@users.noreply.github.com>
2022-08-30 18:12:17 +0200

    Create config.php

---
+++ panel/ESX Version/ug-webAdminPanel/app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+    /**
+     * ug-webAdminPanel
+     * 
+     * This Project is licensed under the MIT License!
+     * Our team will not give support for people who
+     * will edit the source code of this project or/and
+     * have outdated versions of ug-webAdminPanel
+     * 
+     * Copyright (C) 2022 UgScripts. All Rights Reserved!
+     */
+    $Config['WebsiteTitle'] = "Ug Web Admin Panel";
+
+    $Config['IP'] = "127.0.0.1";
+    $Config['Port'] = 30120;
+    $Config['Password'] = "CONFIGURADORES";
+    $Config['Log_File'] = "output.txt";   
+?>
