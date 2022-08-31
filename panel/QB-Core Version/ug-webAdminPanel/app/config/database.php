66e631256b3d002473ffcd2620422993ecc3a114
UgScripts <112483241+UgScripts@users.noreply.github.com>
2022-08-31 15:45:45 +0200

    Add files via upload

---
+++ panel/QB-Core Version/ug-webAdminPanel/app/config/database.php
@@ -0,0 +1,19 @@
+<?php 
+    /**
+     * Database Config for Panel
+     */
+    $DataBase['DB_WebPanel_Host'] = "localhost";
+    $DataBase['DB_WebPanel_Port'] = 3306;
+    $DataBase['DB_WebPanel_Username'] = "root";
+    $DataBase['DB_WebPanel_Password'] = "";
+    $DataBase['DB_WebPanel_Name'] = "ug-webAdminPanel";
+
+    /**
+     * Database Config for Server
+     */
+    $DataBase['DB_Server_Host'] = "localhost";
+    $DataBase['DB_Server_Port'] = 3306;
+    $DataBase['DB_Server_Username'] = "root";
+    $DataBase['DB_Server_Password'] = "";
+    $DataBase['DB_Server_DBName'] = "ugscriptsqbcore";
+?>
